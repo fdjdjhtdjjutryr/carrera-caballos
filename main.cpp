55df1761a2cc65dde35c6ee38652b8eeb2fed384
JeremyM2 <85074733+JeremyM2@users.noreply.github.com>
2026-04-12 21:26:03 -0400

    Version1
    subiendo archivo a GitHub, trabajo de manera local a través de discord

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include <ncurses.h>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include "caballo.cpp"
 
-void inicio();
+int main() {
+    srand(time(NULL));
+    initscr(); cbreak(); noecho(); curs_set(0);
 
-int main(){
-    initscr();
-    noecho();
-    curs_set(0);
+    Caballo c[4];
+    strcpy(c[0].nombre, "Caballo 1"); c[0];
+    strcpy(c[1].nombre, "Caballo 2");   c[1];
+    strcpy(c[2].nombre, "Caballo 3");    c[2];
+    strcpy(c[3].nombre, "Caballo 4");    c[3];   
 
-    inicio();
+    int caballo_ganador = correr(c, 4);
+    ganador(c[caballo_ganador]);
 
-    refresh();
-    getch();
     endwin();
     return 0;
-}
-
-void inicio(){
-    
-    printw("Quiero agua\n");
-    
 }
